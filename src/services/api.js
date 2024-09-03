d3e195f551a8ced1b96b4e2aa62bbd95170d35bd
AdrianJonathanMB <adrianbonfire999@gmail.com>
2024-09-03 14:20:18 -0300

    feat: .env api url add

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,18 @@ import axios from 'axios';
 
 var token = localStorage.getItem('token');
 
+const Url = () => {
+  if (import.meta.env.MODE === 'development') {
+    console.log("development")
+    return import.meta.env.VITE_URL_API
+  } else if (import.meta.env.MODE === 'production') {
+    console.log("production")
+    return import.meta.env.VITE_LOCAL_URL_API
+  }
+}
+
 const api = axios.create({
-  baseURL: "https://bonfire-status-api.onrender.com/v1",
+  baseURL: Url(),
   headers: {
     'Content-Type': 'application/json',
     'authorization': token
